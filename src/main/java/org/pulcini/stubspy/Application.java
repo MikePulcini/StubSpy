b450e94f1a4e0cfcf6d012cd1f4ea052b0183f60
Mike Pulcini <mike.pulcini@idmworks.com>
2016-06-27 23:26:18 -0400

    Add initial boilerplate for Spring Boot

---
+++ src/main/java/org/pulcini/stubspy/Application.java
@@ -0,0 +1,20 @@
+package org.pulcini.stubspy;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
